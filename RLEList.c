423586446aa2c761a086890d4760b3012b325de3
taligrayzel <tali.grayzel@campus.technion.ac.il>
2022-04-17 12:13:26 +0300

    create function

--- RLEList.c
+++ RLEList.c
@@ -1,6 +1,19 @@
 #include "RLEList.h"
-struct RLEList_t{
-    //TODO: implement
-};
+#include <stdlib.h>
+typedef struct RLEList_t{
+    char *s;
+    struct RLEList *next;
+}*RLEList_node;
 
 //implement the functions here
+RLEList RLEListCreate()
+{
+    RLEList_node new_node = malloc(sizeof(new_node));
+    if(!new_node)
+    {
+        return NULL;
+    }
+    new_node->s = NULL;
+	new_node->next = NULL;
+	return new_node;
+}
